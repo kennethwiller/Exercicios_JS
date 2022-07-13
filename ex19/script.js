9a88115273a8f719b423e56cc4aae2b32b5896d6
kennethwiller <81170337+kennethwiller@users.noreply.github.com>
2022-07-13 09:21:37 -0300

    correção script ex19
    Correção de lógica no script do ex19

--- ex19/script.js
+++ ex19/script.js
@@ -1,6 +1,6 @@
 const frm = document.querySelector("form");
-const resp1 = document.getElementById("#resp1");
-const resp2 = document.getElementById("#resp2");
+const resp1 = document.getElementById("resp01");
+const resp2 = document.getElementById("resp02");
 
 let resposta = "";
 let numContas = 0;
@@ -11,14 +11,15 @@ frm.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const descricao = frm.descricao.value;
-    const valor = (frm.valor.value);
-
-    numContas++;
+    const valor = Number(frm.valor.value);
+    
     valorTotal = valorTotal + valor;
-    resposta = `${resposta} ${descricao} - R\$: ${valor}`;
+    numContas++;
+    
+    resposta = `${resposta} ${descricao} - R\$: ${valor} \n`;
 
     resp1.innerText = `${resposta} --------------------------`;
-    resp2.innerText = `${numContas} Conta(s) - Total R$: ${valorTotal.toFixed(2)}`;
+    resp2.innerText = `${numContas} Conta(s) - Total R$: ${valorTotal}`;
 
     frm.descricao.value = "";
     frm.valor.value = "";
